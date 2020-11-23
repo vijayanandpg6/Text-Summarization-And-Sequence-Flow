a4205f14403eccb2ffde70625cf05a654347a5ca
Vijayanand Gajapathy <Vijayanand.pg6@gmail.com>
2020-11-23 18:22:04 +0530

    Read data using pandas

--- PythonScripts/convertToPOSjson.py
+++ PythonScripts/convertToPOSjson.py
@@ -15,6 +15,20 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 
+
+#using panda read function
+file = open("D:\TextManipulationRepo\ProcessFiles\SummarizedTextDocument.txt","r")
+contents= file.read()
+print(contents)
+from io import StringIO
+TESTDATA = StringIO(contents.replace("^","-"))
+
+data = pd.read_csv(TESTDATA,sep=";",lineterminator='-',names=['body_text','label'])
+# print(file.read())
+#data = pd.read_csv('M10033.tsv', sep=',',lineterminator='-', names=['body_text'])
+print(data.head())
+
+
 resultstr=resultstr+"]}"   
 temp1= str(resultstr).replace("'", '"')
 print(temp1)
