399e0cd253a9236b1251797cc16983b53791022d
Vijayanand Gajapathy <Vijayanand.pg6@gmail.com>
2020-12-27 18:24:32 +0530

    Obtain word frequencies

--- PythonScripts/nlpShorteningByText.py
+++ PythonScripts/nlpShorteningByText.py
@@ -28,6 +28,22 @@ strContent = re.sub(r'\s+', ' ', strContent)
 formatted_strContent = re.sub('[^a-zA-Z]', ' ', strContent )  
 formatted_strContent = re.sub(r'\s+', ' ', formatted_strContent)  
 
+sentence_list = nltk.sent_tokenize(strContent) 
+print("\n\n")
+print("Sentence list **************************************************")
+print(sentence_list)
+print("\n\n")
+
+stopwords = nltk.corpus.stopwords.words('english')
+
+word_frequencies = {}  
+for word in nltk.word_tokenize(formatted_strContent):  
+    if word not in stopwords:
+        if word not in word_frequencies.keys():
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
+
 summary = ' '.join(summary_sentences)  
 sum2=summary.replace(".",'.^')
 print(sum2)  
