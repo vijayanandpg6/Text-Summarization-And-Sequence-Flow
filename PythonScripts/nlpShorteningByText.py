a211f134184a5358bb318c4e2a44e854f01f273f
Vijayanand Gajapathy <Vijayanand.pg6@gmail.com>
2020-12-28 18:25:01 +0530

    Obtain proper sentence scores

--- PythonScripts/nlpShorteningByText.py
+++ PythonScripts/nlpShorteningByText.py
@@ -44,6 +44,24 @@ for word in nltk.word_tokenize(formatted_strContent):
         else:
             word_frequencies[word] += 1
 
+maximum_frequncy = max(word_frequencies.values())
+
+for word in word_frequencies.keys():  
+    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+
+
+sentence_scores = {}  
+for sent in sentence_list:  
+    for word in nltk.word_tokenize(sent.lower()):
+        if word in word_frequencies.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
+
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
 summary = ' '.join(summary_sentences)  
 sum2=summary.replace(".",'.^')
 print(sum2)  
